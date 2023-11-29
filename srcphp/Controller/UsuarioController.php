885ce3e1c03c2e89f13d88acc3af921e3acd0e33
Angel Murillo <aangelmurv@gmail.com>
2023-11-29 08:38:07 -0600

    Actualizar query

--- srcphp/Controller/UsuarioController.php
+++ srcphp/Controller/UsuarioController.php
@@ -19,17 +19,13 @@ class UsuarioController
 
     public function verUsuariosAdmin()
     {
-        $db = Table::query("SELECT
+        $db = Table::query("
+        SELECT
         CONCAT(usuarios.nombre_usuario, ' ', usuarios.apellido_p_usuario) AS Nombre,
         usuarios.telefono_usuario AS Telefono,
         usuarios.email_usuario AS Email,
         roles.nombre_rol AS Rol,
-        COUNT(pedidos_clientes.id_pedido) AS PedidosRealizados
-    FROM usuarios
-    INNER JOIN roles ON usuarios.id_rol = roles.id
-    LEFT JOIN pedidos_clientes ON usuarios.id = pedidos_clientes.id_usuario
-    GROUP BY usuarios.id
-    ORDER BY Nombre;");
+        SUM(pedidos.estado_pedido = 'Entregado') AS PedidosRealizados FROM usuarios INNER JOIN roles ON usuarios.id_rol = roles.id LEFT JOIN pedidos_clientes ON usuarios.id = pedidos_clientes.id_usuario LEFT JOIN pedidos ON pedidos.id = pedidos_clientes.id_pedido GROUP BY usuarios.id ORDER BY Nombre;");
         $db = new Success($db);
 
         $db->Send();
