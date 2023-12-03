d93240e85f1c6260644cc2d7b30f1360292a2f97
Angel Murillo <aangelmurv@gmail.com>
2023-12-03 15:26:34 -0600

    Modificacion de la consulta de corte de caja

--- srcphp/Controller/PedidoController.php
+++ srcphp/Controller/PedidoController.php
@@ -48,17 +48,12 @@ class PedidoController
     public function vercortedecaja()
     {
         try {
-            $caj = Table::query("SELECT
+            $caj = Table::query("SELECT pedidos.id AS Pedido_ID,
             CASE
                 WHEN pedidos.nombre_cliente_pedido IS NOT NULL THEN pedidos.nombre_cliente_pedido
                 ELSE CONCAT(usuarios.nombre_usuario, ' ', usuarios.apellido_p_usuario)
-            END AS Nombre,
-            pedidos.fecha_realizado_pedido AS Fecha,
-            pedidos.estado_pedido AS Estado,
-            usuarios.telefono_usuario AS Telefono,
-            productos.nombre_producto AS Producto,
-            detalles_pedido.cantidad_producto AS Cantidad,
-            detalles_pedido.subtotal_pedido AS Subtotal,
+            END AS Nombre, pedidos.fecha_realizado_pedido AS Fecha, pedidos.estado_pedido AS Estado, usuarios.telefono_usuario AS Telefono,
+            detalles_pedido.nombre_producto AS Producto, detalles_pedido.cantidad_producto AS Cantidad, detalles_pedido.subtotal_pedido AS Subtotal,
             (SELECT SUM(subtotal_pedido) FROM detalles_pedido WHERE id_pedido = pedidos.id) AS Total
         FROM pedidos
         LEFT JOIN pedidos_clientes ON pedidos.id = pedidos_clientes.id_pedido
