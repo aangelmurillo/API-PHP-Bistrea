cef4a32f481953b2bb8ff5ad10588dcf39175050
frankx00 <frank.atrh0905@gmail.com>
2023-12-09 17:25:28 -0600

    Se puso el color y tipo de letra al boton que se mnada la correo electronico

--- srcphp/Controller/enviar_correo.php
+++ srcphp/Controller/enviar_correo.php
@@ -7,13 +7,13 @@ function enviarCorreo($destinatario, $codigo) {
     $mail = new PHPMailer(true);
 
     try {
-        $mail->isSMTP();  //Este es el protoocolo [ara enviar correo elecrinicos]
-        $mail->Host = 'smtp.gmail.com';  //Esto es la conexion del servidor esmtp
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'bistreacoffeecakes@gmail.com';
         $mail->Password = 'qogk ocrk rtsz knbl';
-        $mail->SMTPSecure = 'tls';   //esto es ocmo seguridad
-        $mail->Port = 587;  //este es el puerto comun de tls
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
 
         $mail->setFrom('bistreacoffeecakes@gmail.com', 'Bistrea');
         $mail->addAddress($destinatario);
@@ -21,9 +21,22 @@ function enviarCorreo($destinatario, $codigo) {
         $mail->isHTML(true);
         $mail->CharSet = 'UTF-8';
         $mail->Subject = 'Código de Verificación';
-        $mail->Body = "Tu código de verificación es: $codigo";
+
+        // Incluye un formulario y un botón en lugar del código de verificación
+        $formulario = "
+    <form action='http://localhost/PRUEBAS/phpMailer/redireccion.php' method='get'>
+        <input type='hidden' name='codigo' value='$codigo'>
+        <br>
+        <button style='background-color: #889E89; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 4px; font-family: \"Roboto Mono\", monospace;'>Haz clic aquí para validar tu cuenta</button>
+    </form>
+    ";
+
+
+
+        $mail->Body = "Para verificar tu registro $formulario";
 
         $mail->send();
+        
     } catch (Exception $e) {
         echo "Error al enviar el correo: {$mail->ErrorInfo}";
     }
