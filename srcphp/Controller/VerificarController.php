d959a7641bf63998577b348aec5a5b6abd451799
Angel Murillo <aangelmurv@gmail.com>
2023-12-09 19:14:57 -0600

    Ejemplo

--- srcphp/Controller/VerificarController.php
+++ srcphp/Controller/VerificarController.php
@@ -10,7 +10,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require ("../../vendor/autoload.php");
 
 class VerificarController {
     public function enviar() {
