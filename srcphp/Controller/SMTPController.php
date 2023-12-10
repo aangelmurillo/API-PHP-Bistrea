4ddca3a9b517d257da5c6d93f39685c41bd7b797
Angel Murillo <aangelmurv@gmail.com>
2023-12-09 23:01:52 -0600

    Ejemplo

--- srcphp/Controller/SMTPController.php
+++ srcphp/Controller/SMTPController.php
@@ -9,6 +9,8 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+require("../../vendor/autoload.php");
+
 class SMTPController {
 
     public function ejemploSMTP() {
