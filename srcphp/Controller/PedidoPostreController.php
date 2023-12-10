da72c11de357b811c525bbe8c9cc4a836c81a483
JackDLC327 <you@example.com>
2023-12-10 10:35:44 -0600

    PedidoControllerPostre

---
+++ srcphp/Controller/PedidoPostreController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace proyecto\Controller;
+
+use PDO;
+use proyecto\Models\pedido;
+use proyecto\Models\detalle_pedido;
+use proyecto\Models\pedido_cliente;
+use proyecto\Models\producto;
+use proyecto\Response\Success;
+use proyecto\Response\Failure;
+use proyecto\Models\Table;
+
+class PedidoPostreController {
+
+    public function 
+}
